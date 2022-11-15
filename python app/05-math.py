f02b76647c4b79db5b16587dcb48634615ab9b6b
Luo-Yao <luoyao@ti-net.com.cn>
2022-11-15 11:37:48 +0800

    Basic testing

---
+++ python app/05-math.py
@@ -0,0 +1,16 @@
+
+print(2+2)
+
+print(3-2)
+
+print(2*3)
+
+print(2/3)
+
+print(2**3)
+
+print(3**3)
+
+print(1+2*3)
+
+print((1+2)*3)
---
+++ python app/06-birthday.py
@@ -0,0 +1,6 @@
+
+age = 23
+# massage = "happy "+ age +" rd birthday"
+massage = "happy "+ str(age) +" rd birthday"
+
+print(massage)
