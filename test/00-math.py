084409e89f9494df3798c42170e5ff13ead88fca
Luo-Yao <luoyao@ti-net.com.cn>
2022-11-21 23:21:32 +0800

    import module init

---
+++ test/00-math.py
@@ -0,0 +1,7 @@
+
+
+def getSumNumber(x,y):
+    sumNumber= x + y
+    return sumNumber
+
+print(getSumNumber(10,12))
---
+++ test/001-makepizza.py
@@ -0,0 +1,7 @@
+# import pizza
+
+# pizza.make_pizza(10,'robert')
+
+import pizza
+
+pizza.make_pizza(10,'bob')
